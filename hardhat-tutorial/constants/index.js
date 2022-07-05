831c7270614bf44b123cb4ec51ffaf331fdd8a8b
Foster0x <sfoster2010.sf@gmail.com>
2022-07-05 13:04:32 -0400

    updates for opensea

--- hardhat-tutorial/constants/index.js
+++ hardhat-tutorial/constants/index.js
@@ -1,6 +1,9 @@
 // Address of the Whitelist Contract that you deployed
 const WHITELIST_CONTRACT_ADDRESS = "0x9730C217B4107777B4662A48463585A541D38441";
 // URL for the Crypto Devs NFT Metadata
-const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+const METADATA_URL = "https://my-nft-collection-omega.vercel.app/api/";
 
-module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
+module.exports = {
+    WHITELIST_CONTRACT_ADDRESS,
+    METADATA_URL
+};
